51f93d5685b7a2b4edeae972ae3ab95b56c27386
Ralph Goffart Winkin <65244772+Ralph2003@users.noreply.github.com>
2022-09-24 03:37:59 +0900

    darkmode
    modifying the darkmode button so that the function goes through even when user switch pages

--- static/script.js
+++ static/script.js
@@ -1,81 +1,66 @@
 // document.body.style.backgroundColor = 'black';
+function darkmode() {
+    // DARKMODE
+    // localStorage.setItem("theme", "DARK");
 
+    document.querySelector('ul.nav li').style.color = "white";
+    for (let i = 0; i < document.querySelectorAll('ul.nav li a').length; i++) {
+        document.querySelectorAll('ul.nav li a')[i].style.color = "white";
+    }
+    // change general background color to black
+    document.querySelector('body').style.backgroundImage = "url('photos/ina_yakiniku_night_sky.png')";
+    document.querySelector('header').style.background = 'rgba(9, 44, 62, 0.975)';
+    document.querySelector('footer').style.background = 'rgba(9, 44, 62, 0.975)';
+    document.querySelector('footer').style.color = 'white';
+    // projects part
+    for (let i = 0; i < document.querySelectorAll('#projects_list .project').length; i++) {
+        document.querySelectorAll('#projects_list .project_head')[i].style.background = 'black';
+        document.querySelectorAll('#projects_list .project')[i].style.color = "white";
+    }
+}
+function lightmode() {
+   // LIGHTMODE
 
+   document.querySelector('header').style.backgroundColor = 'rgba(234, 210, 171, 0.975)'
+   document.querySelector('body').style.backgroundImage = "url('photos/ina_yakiniku.jpg')";
+   document.querySelector('ul.nav li').style.color = "black";
+   for (let i = 0; i < document.querySelectorAll('ul.nav li a').length; i++) {
+       document.querySelectorAll('ul.nav li a')[i].style.color = "black";
+   }
+   document.querySelector('footer').style.background = 'rgba(234, 210, 171, 0.975)';
+   document.querySelector('footer').style.color = 'grey';
+   for (let i = 0; i < document.querySelectorAll('#projects_list .project').length; i++) {
+       document.querySelectorAll('#projects_list .project_head')[i].style.background = '#efefef';
+       document.querySelectorAll('#projects_list .project')[i].style.color = 'black';
+   }
+}
 
 const checkbox = document.querySelector("#darkmode");
-let button = document.querySelector("#button");
-
-
-
-
-
-// Add darkmode button
-
-// const dbli = document.createElement("li");
-// dbli.classList.add("darkmode");
-// dbli.appendChild(document.createElement("form"));
-// dbli.querySelector("form").appendChild(document.createElement("label"));
-// dbli.querySelector("label").appendChild(document.createElement("input"));
-// dbli.querySelector("label").appendChild(document.createElement("span"));
-// // dbli.innerHTML = "
-// // <form>\
-// // <label>\
-// //     <input>\
-// //     <span></span>\
-// // </label>\
-// // </form>\"
-// document.querySelector(".nav").appendChild(dbli);
-// document.querySelector("label input").type = "checkbox";
-// document.querySelector("label input").id = "darkmode";
-// document.querySelector("label span").classList.add("check");
-
-
+// let button = document.querySelector("#button");
 
 // add event listener to checkbox and change the header background color to black
 checkbox.addEventListener('change', function() {
     if (checkbox.checked) {
-        // change navi colors titles to white
-        document.querySelector('ul.nav li').style.color = "white";
-        for (let i = 0; i < document.querySelectorAll('ul.nav li a').length; i++) {
-            document.querySelectorAll('ul.nav li a')[i].style.color = "white";
-        }
-        // change general background color to black
-        document.querySelector('body').style.backgroundImage = "url('photos/ina_yakiniku_night_sky.png')";
-        document.querySelector('header').style.background = 'rgba(9, 44, 62, 0.975)';
-        document.querySelector('footer').style.background = 'rgba(9, 44, 62, 0.975)';
-        document.querySelector('footer').style.color = 'white';
-        // projects part
-        for (let i = 0; i < document.querySelectorAll('#projects_list .project').length; i++) {
-            document.querySelectorAll('#projects_list .project_head')[i].style.background = 'black';
-            document.querySelectorAll('#projects_list .project')[i].style.color = "white";
-        }
-
+        localStorage.setItem("theme", "DARK");
+        darkmode();
     } else {
-        document.querySelector('header').style.backgroundColor = 'rgba(234, 210, 171, 0.975)'
-        document.querySelector('body').style.backgroundImage = "url('photos/ina_yakiniku.jpg')";
-        document.querySelector('ul.nav li').style.color = "black";
-        for (let i = 0; i < document.querySelectorAll('ul.nav li a').length; i++) {
-            document.querySelectorAll('ul.nav li a')[i].style.color = "black";
-        }
-        document.querySelector('footer').style.background = 'rgba(234, 210, 171, 0.975)';
-        document.querySelector('footer').style.color = 'grey';
-        for (let i = 0; i < document.querySelectorAll('#projects_list .project').length; i++) {
-            document.querySelectorAll('#projects_list .project_head')[i].style.background = '#efefef';
-            document.querySelectorAll('#projects_list .project')[i].style.color = 'black';
-        }
+        localStorage.setItem("theme", "LIGHT");
+        lightmode();
     }
 });
-// function eventHandler(event){
-//     button.addEventListener('click', function() {
-//         if (checkbox.checked) {
-//             document.querySelector('body').style.backgroundColor = 'black';
-//         } else {
-//             document.querySelector('body').style.backgroundColor = 'red';
-//         }
-//     })
-// }
 
-// function myFunction() {
-//     // change body background to red
-//     document.body.style.backgroundColor = 'red';
-// }
+// set local storage so that it stays the same even after reloading
+if (localStorage.getItem("theme") == null) {
+localStorage.setItem("theme", "LIGHT");
+}
+let localData = localStorage.getItem("theme");
+
+if (localData == "LIGHT") {
+    lightmode();
+    // checkbox.checked = false;
+} 
+else {
+    darkmode();
+    // checkbox.checked = true;
+}
+
