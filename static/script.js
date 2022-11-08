faca72e3370a6752e8c48b977763092649d14abd
Ralph Goffart Winkin <65244772+Ralph2003@users.noreply.github.com>
2022-11-08 22:23:44 +0100

    Dark mode finition
    debugging the bug that was putting the footer in light mode when the browser was supposed to put everything in dark mode after remembering the user's settings

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 // document.body.style.backgroundColor = 'black';
 function darkmode() {
     // DARKMODE
-    // localStorage.setItem("theme", "DARK");
+    localStorage.setItem("theme", "DARK");
 
     document.querySelector('ul.nav li').style.color = "white";
     for (let i = 0; i < document.querySelectorAll('ul.nav li a').length; i++) {
@@ -18,6 +18,7 @@ function darkmode() {
         document.querySelectorAll('#projects_list .project')[i].style.color = "white";
     }
 }
+
 function lightmode() {
    // LIGHTMODE
 
@@ -35,17 +36,36 @@ function lightmode() {
    }
 }
 
+function darkmode_new() {
+    document.querySelector('body').style.backgroundImage = "url('photos/ina_yakiniku_night_sky.png')";
+    document.querySelector('body').classList.add('dark-theme');
+    // document.querySelector('header').classList.add('dark-theme');
+    // document.querySelector('footer').classList.add('dark-theme');
+    // document.querySelector('section').classList.add('dark-theme');
+    // document.querySelector('projects_list').classList.add('dark-theme');
+}
+
+function lightmode_new() {
+    document.querySelector('body').style.backgroundImage = "url('photos/ina_yakiniku.jpg')";
+    document.querySelector('body').classList.remove('dark-theme');
+    // document.querySelector('header').classList.remove('dark-theme');
+    // document.querySelector('footer').classList.remove('dark-theme');
+    // document.querySelector('section').classList.remove('dark-theme');
+    // document.querySelector('projects_list').classList.remove('dark-theme');
+}
 const checkbox = document.querySelector("#darkmode");
 // let button = document.querySelector("#button");
 
 // add event listener to checkbox and change the header background color to black
 checkbox.addEventListener('change', function() {
+    // document.querySelector('header').classList.toggle('dark-theme');
     if (checkbox.checked) {
         localStorage.setItem("theme", "DARK");
-        darkmode();
+        // darkmode();
+        darkmode_new();
     } else {
         localStorage.setItem("theme", "LIGHT");
-        lightmode();
+        lightmode_new();
     }
 });
 
@@ -56,11 +76,15 @@ localStorage.setItem("theme", "LIGHT");
 let localData = localStorage.getItem("theme");
 
 if (localData == "LIGHT") {
-    lightmode();
-    // checkbox.checked = false;
+    // lightmode();
+    checkbox.checked = false;
+    lightmode_new();
+
 } 
-else {
-    darkmode();
-    // checkbox.checked = true;
+if (localData == "DARK") {
+    checkbox.checked = true;
+    // darkmode();
+    // document.querySelector('footer').classList.add('dark-theme');
+    darkmode_new();
 }
 
